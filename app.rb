7d690041de581abd5b1097bd5e4a59d7804cfdc6
Clare Pinder <Clare.pinder@live.co.uk>
2019-02-18 16:56:12 +0000

    added parameter name

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 # set :session_secret, 'super secret'
 require 'sinatra'
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
