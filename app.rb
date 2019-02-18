d0659027b02a0322fe597953fafb0dfb84209185
Clare Pinder <Clare.pinder@live.co.uk>
2019-02-18 16:44:26 +0000

    moved ruby code into the route

--- app.rb
+++ app.rb
@@ -1,23 +1,7 @@
 # set :session_secret, 'super secret'
 require 'sinatra'
 
-get '/' do
-  "Hello world"
-  "hello hello hello"
-end
-
-get '/secret' do
-  "Oi"
-end
-
-get '/secret2' do
-  "hello hello hello"
-end
-
-get '/123' do
-  "testing testing 123"
-end
-
 get '/cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
